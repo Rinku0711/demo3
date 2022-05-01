af81cf880b9d5f1f32634bc9de767d89ef32c48f
Rinku <rinkufernandes11@mail.com>
2022-05-01 19:24:09 +0100

    classadd

---
+++ src/table.java
@@ -0,0 +1,9 @@
+public class table {
+    public static void main(String[] args) {
+
+        for (int i = 2; i<=2; i++){
+            for(int k = 1; k<=10; k++)
+                System.out.println(i + "*"+k+"="+i*k);
+        }
+    }
+}
